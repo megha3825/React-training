9e751ac47ee9cf769bf121d21ee480780c7fd3a0
Megha Sharma <megha.sharma@qed42.com>
2025-01-21 12:02:17 +0530

    Updated main file.

--- calculator-application/calculator-app/src/main.jsx
+++ calculator-application/calculator-app/src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/React-training/calculator-application/calculator-app/">
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
